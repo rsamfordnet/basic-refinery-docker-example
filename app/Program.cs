c5ca96c8b236fcd7cf95ab4086351c33dc501429
Rayn Samford <rsamfordnet@gmail.com>
2023-08-03 16:37:59 +0100

    Added otel debugging

--- app/Program.cs
+++ app/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<LoggingOpenTelemetryListener>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,7 +18,10 @@ builder.Services.AddOpenTelemetry()
         .AddOtlpExporter()
     );
 
+
+
 var app = builder.Build();
+var listener = app.Services.GetRequiredService<LoggingOpenTelemetryListener>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,3 +37,23 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public class LoggingOpenTelemetryListener : EventListener
+{
+    private readonly ILogger<LoggingOpenTelemetryListener> _logger;
+
+    public LoggingOpenTelemetryListener(ILogger<LoggingOpenTelemetryListener> logger)
+    {
+        _logger = logger;
+    }
+    protected override void OnEventSourceCreated(EventSource eventSource)
+    {
+        if (eventSource.Name.StartsWith("OpenTelemetry"))
+            EnableEvents(eventSource, EventLevel.Error);
+    }
+
+    protected override void OnEventWritten(EventWrittenEventArgs eventData)
+    {
+        _logger.LogWarning(eventData.Message, eventData.Payload?.Select(p => p?.ToString())?.ToArray());
+    }
+}
